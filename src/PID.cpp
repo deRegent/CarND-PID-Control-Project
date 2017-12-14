683739fb52f890899ae37126ea9c5fe30302e4a9
Dmitry Zaganich <dmitryzaganich@gmail.com>
2017-12-14 08:31:44 +0200

    update with QA code

--- src/PID.cpp
+++ src/PID.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
+* Done: Complete the PID class.
+* This class was copied from lesson resources: Self-Driving Car Project Q&A | PID Controller
+* https://www.youtube.com/watch?v=YamBuzDjrs8&list=PLAwxTw4SYaPnfR7TzRZN-uxlxGbqxhtm2&index=4
+* which was sent to me by my mentor
 */
 
 PID::PID() {}
@@ -11,11 +14,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+	d_error = cte - p_error;
+	p_error = cte;
+	i_error += cte;
 }
 
 double PID::TotalError() {
+	return Kp*p_error + Ki*i_error + Kd*d_error;
 }
 
