6f19f924e6817bef7ef5f220ce9c438d2f6f8b72
Dmitry Zaganich <dmitryzaganich@gmail.com>
2017-12-18 11:32:01 +0200

    fix lambda

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 
   pid.Init(init_Kp, init_Ki, init_Kd);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&pid, &throttle](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
